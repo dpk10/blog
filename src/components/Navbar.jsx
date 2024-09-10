d130149b76200dae246cce6f67eeb212d563d213
himanshu <himanshuchauhan696@gmail.com>
2024-09-10 15:44:51 +0530

    Navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,21 +12,21 @@ const Navbar = () => {
   
       
       <nav className='bg-blue-300 p-10 shadow-md'>
-        <div className='container mx-auto flex justify-between items-center'>
+        <div className='container mx-auto flex justify-between items-center'> 
 
-        <Link to="/">
-        {/* <img src={logo} alt="logo" className="h-12 w-auto"/> */}
-        </Link>
-        <div className='space-x-6'>
+       <Link to="/">
+       {/* <img src={logo} alt="logo" className="h-12 w-auto"/>  */}
+         </Link> 
+        {/* <div className='space-x-6'> */}
           {/* <link to="/">Home</link> */}
-          <link to="/authorform">Author Form</link>
-          <link to="/blogform">Blog Form</link>
-          <link to="/bloglist">Blog List </link>
+          <Link to="/authorform">Author Form</Link>
+          <Link to="/blogform">Blog Form</Link>
+          <Link to="/bloglist">Blog List </Link>
     
-        </div>
+        {/* </div> */}
         {/* <button className='bg-cyan-500 text-white px-6 py-2 rounded-lg font-semibold hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600' onClick={handleclisk}>Blog Form</button> */}
 
-        </div>
+      </div> 
 
       </nav>
     
